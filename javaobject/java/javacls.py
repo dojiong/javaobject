9d8778d8da299728ae32fe8bce4066b14360cefa
lo <lodevil@live.cn>
2013-11-06 11:14:35 +0800

    java object copyfrom

--- javaobject/java/javacls.py
+++ javaobject/java/javacls.py
@@ -8,27 +8,27 @@ class JavaClassMeta(six.with_metaclass(Prepareable, type)):
     def __prepare__(name, bases):
         return OrderedDict()
 
-    def __new__(self, name, bases, clsdict):
-        if '__javaclass__' not in clsdict:
+    def __new__(self, name, bases, attrs):
+        if '__javaclass__' not in attrs:
             raise TypeError('missing __javaclass__')
 
         fields = OrderedDict()
-        for key, field in clsdict.items():
+        for key, field in attrs.items():
             if isinstance(field, BaseField):
                 fields[key] = field
                 if callable(field.default):
-                    clsdict[key] = field.default()
+                    attrs[key] = field.default()
                 else:
-                    clsdict[key] = field.default
-        clsdict['__fields__'] = fields
-        if '__suid__' not in clsdict:
-            clsdict['__suid__'] = 0
+                    attrs[key] = field.default
+        attrs['__fields__'] = fields
+        if '__suid__' not in attrs:
+            attrs['__suid__'] = 0
 
-        if '__classflag__' in clsdict and clsdict['__classflag__'] > 0xFF:
-            raise TypeError('invalid class flag: 0x%X' % clsdict['__classflag__'])
+        if '__classflag__' in attrs and attrs['__classflag__'] > 0xFF:
+            raise TypeError('invalid class flag: 0x%X' % attrs['__classflag__'])
 
-        cls = type.__new__(self, name, bases, clsdict)
-        cls._classes[clsdict['__javaclass__']] = cls
+        cls = type.__new__(self, name, bases, attrs)
+        cls._classes[attrs['__javaclass__']] = cls
         return cls
 
 
@@ -61,3 +61,9 @@ class JavaClass(object):
     @classmethod
     def __frompy__(self, v):
         return v
+
+    def copyfrom(self, obj):
+        if not isinstance(obj, type(self)):
+            raise ValueError('type not equal')
+        for k, field in self.__fields__.items():
+            setattr(self, k, getattr(obj, k))
