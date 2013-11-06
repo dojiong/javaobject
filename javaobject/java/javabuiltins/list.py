508a2fbe5b11fc10132bd268f8010333a2d68f00
lo <lodevil@live.cn>
2013-11-06 11:53:58 +0800

    x

--- javaobject/java/javabuiltins/list.py
+++ javaobject/java/javabuiltins/list.py
@@ -3,3 +3,12 @@ from ..javacls import JavaClass
 
 class List(JavaClass):
     __javaclass__ = 'java.util.List'
+
+    @classmethod
+    def __frompy__(cls, v):
+        ArrayList = JavaClass.resolve('java.util.ArrayList')
+        if isinstance(v, list):
+            return ArrayList(v)
+        elif isinstance(v, tuple):
+            return ArrayList(v)
+        raise ValueError('invalid List')
